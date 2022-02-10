0bc5de6ef53b86749812371528aa2c3903cd4220
Leandro Loureiro <leandromltec@hotmail.com>
2022-02-09 22:11:15 -0300

    TASK/QUIZ-5 Inclusão do parametrô categoria no Repository getQuestion

--- lib/features/domain/respositories/question_repository.dart
+++ lib/features/domain/respositories/question_repository.dart
@@ -3,5 +3,5 @@ import 'package:appquiz/features/domain/entities/question_entity.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class IQuestionRepository {
-  Future<Either<Failure, QuestionEntity>> getQuestion();
+  Future<Either<Failure, QuestionEntity>> getQuestion(String category);
 }
